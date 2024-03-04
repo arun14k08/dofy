1d003ddb6f8e5faab22a6a225464bfe12d726dd3
ARUN KUMAR D <arun14k08@gmail.com>
2024-03-04 14:47:01 +0530

    Cleaned workspace

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import CardsContainer from "./components/CardsContainer";
 import Footer from "./components/Footer";
-import { Card, FAQ, FAQtemplate, InBoxContents } from "./components/index";
+import { FAQ, InBoxContents } from "./components/index";
 
 const App = () => {
-  return (
-    <>
-      <div className="w-full flex flex-col justify-center items-center">
-        <CardsContainer />
-      <InBoxContents />
-      </div>
-      <FAQ />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div className="w-full flex flex-col justify-center items-center">
+                <CardsContainer />
+                <InBoxContents />
+            </div>
+            <FAQ />
+            <Footer />
+        </>
+    );
 };
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Card from "./Card";
 import FAQtemplate from "./FAQtemplate";
 import InBoxContents from "./InBoxContents";
 import CardTag from "./CardTag";
+import CardsContainer from "./CardsContainer";
+import Footer from "./Footer";
 export {
     Card,
     CardsContainer,
     Footer,
+    FAQ,
+    FAQtemplate,
     InBoxContents,
     CardTag,
 }
