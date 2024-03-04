d14b7ba88795519593468b609b0d9231ef93f98b
ARUN KUMAR D <arun14k08@gmail.com>
2024-03-04 14:26:00 +0530

    FEAT: created Cardtag component

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Card from "./Card"
 import CardsContainer from "./CardsContainer";
 import Footer from "./Footer";
 import InBoxContents from "./InBoxContents";
+import CardTag from "./CardTag";
 export {
     Card,
     CardsContainer,
     Footer,
     InBoxContents,
+    CardTag,
 }
