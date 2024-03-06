daaa66955fc42b55bc1752e22e42d208ccf8d945
ARUN KUMAR D <arun14k08@gmail.com>
2024-03-06 11:33:41 +0530

    FEAT: added button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+    return (
+        <div className="px-8 py-3 bg-[#EA002A] rounded w-max cursor-pointer">
+            <p className="text-white font-medium text-[14px] leading-[22px]">
+                Explore More
+            </p>
+        </div>
+    );
+};
+
+export default Button;
--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import InBoxContents from "./InBoxContents";
 import CardTag from "./CardTag";
 import CardsContainer from "./CardsContainer";
 import Footer from "./Footer";
+import Button from "./Button";
 export {
     Card,
     CardsContainer,
@@ -13,4 +14,5 @@ export {
     FAQtemplate,
     InBoxContents,
     CardTag,
+    Button,
 }
